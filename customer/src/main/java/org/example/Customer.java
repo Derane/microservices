09e4e31a02f31d8a5f12967fa8a8f52b222714b9
Valerii Sierhieiev <vsierhieiev-its24@lll.kpi.ua>
2023-10-04 22:04:21 +0300

    Initial commit

---
+++ customer/src/main/java/org/example/Customer.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(schema = "public", name = "customers")
+public class Customer {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	@Column(name = "firstname")
+	private String firstName;
+	@Column(name = "lastname")
+	private String lastName;
+
+	private String email;
+}
---
+++ customer/src/main/java/org/example/CustomerRegistrationRequest.java
@@ -0,0 +1,5 @@
+package org.example;
+
+public record CustomerRegistrationRequest(String firstName, String lastName, String email) {
+	
+}
