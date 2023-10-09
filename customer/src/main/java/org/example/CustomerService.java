e43e1490fb756d640408956b4626ba44ee70335f
Valerii Sierhieiev <vsierhieiev-its24@lll.kpi.ua>
2023-10-09 21:59:10 +0300

    add register

--- customer/src/main/java/org/example/CustomerService.java
+++ customer/src/main/java/org/example/CustomerService.java
@@ -1,16 +1,27 @@
 package org.example;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
-public record CustomerService(CustomerRepository customerRepository) {
+public record CustomerService(CustomerRepository customerRepository, RestTemplate restTemplate) {
+
 	public void registerCustomer(CustomerRegistrationRequest request) {
 		Customer customer = Customer.builder()
 				.firstName(request.firstName())
 				.lastName(request.lastName())
 				.email(request.email())
 				.build();
-		customerRepository.save(customer);
+		customerRepository.saveAndFlush(customer);
+		FraudCheckResponse response = restTemplate.getForObject(
+				"http://localhost:8081/api/v1/fraud-check/{customerId}",
+				FraudCheckResponse.class,
+				customer.getId()
+		);
+		if (response.isFraudster()) {
+			throw new IllegalStateException("Customer is a fraudster!");
+		}
+
 	}
 
 }
