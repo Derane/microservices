8eb20e21564a60461bbd28b7899a12c5da7071a3
Valerii Sierhieiev <vsierhieiev-its24@lll.kpi.ua>
2023-10-10 23:21:38 +0300

    add zipkin

--- fraud/src/main/java/org/example/FraudController.java
+++ fraud/src/main/java/org/example/FraudController.java
@@ -9,13 +9,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/fraud-check")
+@RequestMapping("/api/v1/fraud-check/")
 @RequiredArgsConstructor
 @Slf4j
 public class FraudController {
 	private final FraudCheckService fraudCheckService;
 
-	@GetMapping("{customerId}")
+	@GetMapping("/{customerId}")
 	public FraudCheckResponse isFraudster(@PathVariable Integer customerId) {
 		boolean isFraudulentCustomer = fraudCheckService.isFraudulent(customerId);
 		log.info("Customer {} is fraudulent: {}", customerId, isFraudulentCustomer);
